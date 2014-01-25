1131176ec1225c9b9155bcd254eca6bba67748bc
Anton Karamanov <ataraxer@gmail.com>
2014-01-26 00:46:50 +0400

    Add edit distance calculation in Python

---
+++ python/edit-distance.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+class NLP:
+    @staticmethod
+    def edit_distance(seq1, seq2):
+        oneago = None
+        thisrow = range(1, len(seq2) + 1) + [0]
+        for x in xrange(len(seq1)):
+            twoago, oneago, thisrow = oneago, thisrow, [0] * len(seq2) + [x + 1]
+            for y in xrange(len(seq2)):
+                delcost = oneago[y] + 1
+                addcost = thisrow[y - 1] + 1
+                subcost = oneago[y - 1] + (seq1[x] != seq2[y])
+                thisrow[y] = min(delcost, addcost, subcost)
+        return thisrow[len(seq2) - 1]
